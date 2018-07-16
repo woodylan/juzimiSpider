49192d634db516f0de31214175b9c374340c69bf
woodylan <woodylan@126.com>
2018-07-16 14:18:54 +0800

    fix

--- juzimiSpider/settings.py
+++ juzimiSpider/settings.py
@@ -19,7 +19,12 @@ NEWSPIDER_MODULE = 'juzimiSpider.spiders'
 #USER_AGENT = 'juzimiSpider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+
+DOWNLOAD_DELAY = 2
+RANDOMIZE_DOWNLOAD_DELAY = True
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5'
+COOKIES_ENABLED = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- juzimiSpider/spiders/juzimiSpider.py
+++ juzimiSpider/spiders/juzimiSpider.py
@@ -15,18 +15,28 @@ class juzimiSpider(scrapy.Spider):
         for juzi in juzi_list:
             item = JuzimispiderItem()
             content = juzi.xpath('div/a/text()').extract()[0]
-            author = juzi.xpath('div/a/text()').xpath('div[@class="xqjulistwafo"]/a//text()').extract()[0]
-            book = juzi.xpath('div/a/text()').xpath('div[@class="xqjulistwafo"]//span/a//text()').extract()[0]
+            try:
+                author = juzi.xpath('div[@class="xqjulistwafo"]/a//text()').extract()[0]
+            except:
+                author=''
+            try:
+                book = juzi.xpath('div[@class="xqjulistwafo"]//span/a//text()').extract()[0]
+            except:
+                book=''
+            self.log('打印')
             item['content'] = content
             item['author'] = author
             item['book'] = book
             yield item
 
         ## 是否还有下一页
-        next_pages = response.css('li.pager-next a::attr(href)').extract()[0]
-
+        try:
+            next_pages = response.css('li.pager-next a::attr(href)').extract()[0]
+        except:
+            next_pages= []
+        
         if next_pages:
-            next_page = 'https://www.juzimi.com' + next_pages.extract()
+            next_page = 'https://www.juzimi.com' + next_pages
             self.log('page_url: %s' % next_page)
             ## 将 「下一页」的链接传递给自身，并重新分析
             yield scrapy.Request(next_page, callback=self.parse)
